61f07aacc977668d95af9f1c09b9e80fcacc25b9
tnt <45692264+tuna2134@users.noreply.github.com>
2021-12-04 22:09:59 +0900

    [Add] get_role

--- discord_api/guild.py
+++ discord_api/guild.py
@@ -1,11 +1,20 @@
 from .channels import TextChannel
+from .role import Role
 
 class Guild:
     def __init__(self, client, data):
         self.name = data["name"]
         self.id = data["id"]
         self.description = data["description"]
+        self.roles = [Role.from_dict(role) for role in data["roles"]]
         self.text_channels = [TextChannel.from_dict(client, i) for i in data["channels"] if i["type"] == 0]
+
+    def get_role(self, id:int):
+        role = None
+        for role in self.roles:
+            if role.id == id:
+                break
+        return role
         
     @classmethod
     def from_dict(cls, client, data):
