b66141a497ca329a26ea073292eddf4f0280f5e2
tnt <45692264+tuna2134@users.noreply.github.com>
2021-12-04 22:10:58 +0900

    [add] await member.add_role(role)

--- discord_api/member.py
+++ discord_api/member.py
@@ -1,10 +1,16 @@
+from .role import Role
+
 class Member:
     def __init__(self, client, guild, user:dict):
+        self.client = client
         self.guild = guild
         self.name = user["username"]
         self.id = user["id"]
         self.discriminator = user["discriminator"]
         self.avatar = user["avatar"]
+
+    async def add_role(self, role:Role):
+        await self.client.http.add_role(self, role)
 
     @classmethod
     def from_dict(cls, client, guild, data):
