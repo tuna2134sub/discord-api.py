0c655710539308fb61f2d75abc6aa75e103a0738
tnt <45692264+tuna2134@users.noreply.github.com>
2021-12-04 22:11:34 +0900

    Update role.py

--- discord_api/role.py
+++ discord_api/role.py
@@ -1,1 +1,15 @@
+class Role:
+    def __init__(self, data):
+        self.data = data
 
+    @property
+    def id(self):
+        return self.data["id"]
+
+    @property
+    def name(self):
+        return self.data["name"]
+        
+    @classmethod
+    def from_dict(cls, data):
+        return cls(data)
