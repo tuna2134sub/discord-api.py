72af183ec42efdcf98019c791605df98ce35f572
tnt <45692264+tuna2134@users.noreply.github.com>
2021-12-06 18:15:45 +0900

    コピペミス

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
         ]
     },
     name="discord-api.py",
-    version="0.0.9",
+    version="0.0.10",
     author="DMS",
     author_email="masato190411@gmail.com",
     description="This is discord-api wrapper.",
